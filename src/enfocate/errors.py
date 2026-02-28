52eacb79cf4608fe7a799592150480747585a6f6
Gabo-Skyder8 <gabrigob8@gmail.com>
2026-02-28 13:56:32 -0400

    Delete src/enfocate directory

--- src/enfocate/errors.py
+++
@@ -1,7 +0,0 @@
-class GameConfigError(Exception):
-    """Se lanza cuando la configuración del juego (metadata) está mal formada."""
-    pass
-
-class GameResourcesError(Exception):
-    """Se lanza si el juego intenta cargar recursos que no existen."""
-    pass
