92e7629f19286bcee7c38e419cebf87e6f27abbc
Gabo-Skyder8 <gabrigob8@gmail.com>
2026-02-28 15:21:16 -0400

    FINAL

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from src.juego import MiJuego
 
 
 if __name__ == "__main__":
-    # Se instancia y se corre.
-    # El Core ya sabe qué hacer porque la lógica está en la base.
     game = MiJuego()
+
     game.run_preview()
